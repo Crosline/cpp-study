92b5a68d220435d85470164841c514985eb6e913
Crosline <cagkancaglayanel@gmail.com>
2023-07-11 15:12:54 +0300

    Add: Vector2 struct

--- Cpp-Study/src/Main.cpp
+++ Cpp-Study/src/Main.cpp
@@ -1,7 +1,21 @@
 #include "Log.h"
 #include <iostream>
 
-int Multiply(int a, int b) {
+struct Vector2 {
+	float x, y;
+
+	Vector2()
+		: x(0.0f), y(0.0f) { }
+
+	Vector2(float x, float y) 
+		: x(x), y(y) { }
+
+	Vector2(const Vector2& other)
+		: x(other.x), y(other.y) { }
+};
+
+
+inline int Multiply(int a, int b) {
 	return a * b;
 }
 
