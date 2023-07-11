bacae2ef584e71c5c858f0cbd9d81a831d077a08
Crosline <cagkancaglayanel@gmail.com>
2023-07-11 19:17:50 +0300

    Update: Main for test functionality

--- Cpp-Study/src/Main.cpp
+++ Cpp-Study/src/Main.cpp
@@ -1,6 +1,8 @@
 #include "Log.h"
 #include <iostream>
+#include <string>
 #include "Models/Vector2.h"
+#include "Models/String.h"
 
 
 inline int Multiply(int a, int b) {
@@ -9,13 +11,33 @@ inline int Multiply(int a, int b) {
 
 int main() {
 
-	LogLn("Hello World");
+	std::string hw = std::string("hello world");
+
+	String smallString = "test";
 
 	int a = Multiply(3, 5);
 
-	Vector2 test = Vector2(3, 5) + Vector2(1, 2);
+	{
 
-	std::cout << test;
+		std::shared_ptr<Vector2> test;
+
+		{
+
+			test = std::make_shared<Vector2>(1, 2);
+
+			std::cout << test.get() << std::endl;
+		}
+
+		long xoffset = (long)&((Vector2*)nullptr)->x;
+		long yoffset = (long)&((Vector2*)nullptr)->y;
+
+		std::cout << test.get() << std::endl;
+		std::cout << xoffset << " : " << yoffset << std::endl;
+	}
+
+	std::cout <<  smallString.contains("es") << std::endl;
+	std::cout <<  smallString.contains(String("test")) << std::endl;
+	std::cout << smallString << std::endl;
 
 	std::cin.get();
 
